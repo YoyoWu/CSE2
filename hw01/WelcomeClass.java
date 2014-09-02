272753770e917da784e13314a2afb7a57f6a1a90
yoyowu <yew218@lehigh.edu>
2014-09-02 01:48:30 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+/////////////////////////////////
+//Yulun Wu
+//CSE 002 Sec 110
+///hw01 
+//8/29/2014
+//
+//  Welcome Class Java Program
+//  Welcome Class displays a welcome to the screen with Lehigh network ID, and a tweet-length autobiographic statement. 
+//      first compile the program
+//          javac WelcomeClass.java
+//      then run the program
+//          java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main(String[]args){
+    
+//  print the statement 
+    System.out.println("    ------------");
+    System.out.println("    | Welcome |    ");
+    System.out.println("    -------------");
+    System.out.println("   ^  ^  ^  ^  ^  ^   ");
+    System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("  |-Y--E--W--2--1--8-|");
+    System.out.println("  \\ /\\ /\\ /\\/ \\ /\\ / ");
+    System.out.println("   v  v  v  v  v  v ");
+    System.out.println(" I went back to China this summer. In July, I traveld to Xi'an and visited the Terracotta Warriors.I love Xi'an. ");
+    
+    }
+}
